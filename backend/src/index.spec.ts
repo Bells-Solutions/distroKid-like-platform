91ebb8249b73b3feed7dae9eeb319239d3e92737
Johan Bell <belljohan3@gmail.com>
2025-02-20 16:47:16 +0100

    refactor: update Jest configuration and test files to use single quotes for consistency

--- backend/src/index.spec.ts
+++ backend/src/index.spec.ts
@@ -1,10 +1,10 @@
-import request from "supertest";
-import index from "./index";
+import request from 'supertest';
+import index from './index';
 
-describe("API Tests", () => {
-  it("should return API Running", async () => {
-    const res = await request(index).get("/");
+describe('API Tests', () => {
+  it('should return API Running', async () => {
+    const res = await request(index).get('/');
     expect(res.statusCode).toEqual(200);
-    expect(res.text).toBe("API Running with Auth0 Authentication");
+    expect(res.text).toBe('API Running with Auth0 Authentication');
   });
 });
